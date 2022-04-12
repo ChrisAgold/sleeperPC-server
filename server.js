5fe6d26537ab4414e519a4ee5d5fcc63a5d12cd0
Chris Agold <christophersagold@gmail.com>
2022-04-12 15:34:17 -0500

    added express

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+
+// ROUTE
+app.get('/',(req,res) => {
+    res.send('Welcome!');
+})
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on port ${port}...`);
+});
+
+
+
