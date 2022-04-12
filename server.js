a166d651aec23a7fea43ff8bc2df3ac537985b2d
Chris Agold <christophersagold@gmail.com>
2022-04-12 16:03:24 -0500

    added errors

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-import express from 'express'
+import express from 'express';
 const app = express()
+
+// MIDDLEWARE
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 // ROUTE
 app.get('/',(req,res) => {
+    throw new Error('error')
     res.send('Welcome!');
 })
 
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 
 const port = process.env.PORT || 5000
