94f37679d6a8fc450aac4d939d7ab28ff76c9291
Chris Agold <christophersagold@gmail.com>
2022-04-17 17:34:10 -0500

    week 5 files

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,13 @@
+import {StatusCodes} from 'http-status-codes'
+import {log} from "nodemon/lib/utils";
+
 const errorHandlerMiddleware = (err,req,res,next) => {
     console.log(err);
-    res.status(500).json({msg:err})
+    const defaultError = {
+        StatusCode:StatusCodes.INTERNAL_SERVER_ERROR,
+        msg:'Something went wrong, try again later',
+    }
+    res.status(defaultError.StatusCode).json({msg:err})
 }
 
 export default errorHandlerMiddleware
