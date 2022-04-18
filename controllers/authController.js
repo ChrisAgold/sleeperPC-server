fadf42c7e95fddc152a025f53d014cce3a9821f8
Chris Agold <christophersagold@gmail.com>
2022-04-17 23:27:52 -0500

    add error-handlers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,33 @@
 import User from '../models/User.js'
 import {StatusCodes} from 'http-status-codes'
 
+class CustomAPIError extends Error {
+    constructor(message) {
+        super(message)
+    }
+}
+
+class BADREQUESTERROR extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.NOT_FOUND
+    }
+}
+
 const register = async (req, res) => {
-    const user = await User.create(req.body)
+    const {name,email,password} = req.body
+    if(!name || !email || password) {
+        throw new BADREQUESTERROR('please provide all values')
+    }
+
+    const user = await User.create({name,email,password})
     res.status(StatusCodes.CREATED).json({user})
 }
 
