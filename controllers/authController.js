a05265bade03dc5b27232dd14fe048fdf1326c20
Chris Agold <christophersagold@gmail.com>
2022-04-23 14:02:43 -0500

    added hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,32 @@
 import User from '../models/User.js'
 import {StatusCodes} from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import {BadRequestError} from '../errors/index.js'
 
 const register = async (req, res) => {
-    const {name,email,password} = req.body
+    const {name, email, password} = req.body
 
-    if(!name || !email || !password) {
+    if (!name || !email || !password) {
         throw new BadRequestError('please provide all values')
     }
 
     // CHECK IF USER ALREADY EXIST BASED ON EMAIL
-    const userAlreadyExists = await User.findOne({email})
-    if(userAlreadyExists) {
-        throw new BadRequestError('Email already in use...')
+    const userAlreadyExists = await User.findOne({ email })
+    if (userAlreadyExists) {
+        throw new BadRequestError('Email already in use')
     }
-    const user = await User.create({name,email,password})
-    res.status(StatusCodes.CREATED).json({user})
+    const user = await User.create({ name, email, password })
+
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({
+        user: {
+            email: user.email,
+            lastName: user.lastName,
+            location: user.location,
+            name: user.name,
+        },
+        token,
+        location: user.location,
+    })
 }
 
 const login = async (req, res) => {
